bf325eb736200a48d06e344b783c8cc4f04d07ef
Tilen Kavčič <tilen.kavcic@outlook.com>
2023-02-10 01:06:53 +0100

    Add links

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ document.addEventListener("DOMContentLoaded", function () {
   const mainTextContainer = document.querySelector(".mainTextContainer");
   const mainText = document.querySelector(".mainText");
   const mainTextEnding = document.querySelector(".mainTextEnding");
+  const header = document.querySelector(".header");
 
   if (mainTextContainer) {
     const viewportHeight = window.innerHeight;
     const contentHeight = document.body.offsetHeight;
     const minTranslateX = 90;
-    let transitionPoint = false;
+
+    const emailTransition = minTranslateX + 40;
+    const linksTransition = minTranslateX + 80;
 
     window.addEventListener("scroll", function () {
       let scrollY = window.scrollY;
-      let progress = -scrollY / (contentHeight - viewportHeight);
-      translateX = -Math.min(progress*2, minTranslateX);
+      let progress = scrollY / (contentHeight - viewportHeight);
+      progress *= 20;
+      translateX = progress;
+      translateX = -Math.min(translateX, minTranslateX);
 
-      if (progress >= minTranslateX + 30) {
+      console.log(progress);
+
+      if (progress >= emailTransition) {
         mainTextEnding.style.color = "#fff";
       } else {
         mainTextEnding.style.color = "#000";
+      }
+
+      if (progress >= linksTransition) {
+        header.style.opacity = "100";
+      } else {
+        header.style.opacity = "0";
       }
 
       mainTextContainer.style.transform = `translateX(${translateX}%)`;
