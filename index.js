eee4d4e7099c6565d30affbe30da7ef5212507e1
Tilen Kavčič <tilen.kavcic@outlook.com>
2023-08-25 14:49:48 +0200

    Change background

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const mainHtml = document.querySelector("html");
   const mainTextContainer = document.querySelector(".mainTextContainer");
   const mainText = document.querySelector(".mainText");
   const mainTextEnding = document.querySelector(".mainTextEnding");
@@ -18,12 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", function () {
       let scrollY = window.scrollY;
       let progress = scrollY / (contentHeight - viewportHeight);
+
+      // Animate background gradient
+      mainHtml.style.background = `conic-gradient(from ${progress/20}turn at 0% 0%, #00c476, 20%, #82b0ff, 90%, #00c476)`;
+      mainHtml.style.backgroundAttachment = `fixed`;
+      console.log(progress/9)
+      // Adjust progress value for transitions
       progress *= 80;
       translateX = progress;
       translateX = -Math.min(translateX, minTranslateX);
 
+      // Specify fade in on scroll transitions
       scrollAnim.style.opacity = progress >= scrollAnimTransition ? "0" : "100";
-      mainTextEnding.style.color = progress >= emailTransition ? "inherit" : "#000"
+      mainTextEnding.style.color = progress >= emailTransition ? "inherit" : "#00000000"
       header.style.opacity = progress >= linksTransition ? "100" : "0"
       linksContainer.style.opacity = progress >= linksTransition ? "100" : "0"
 
